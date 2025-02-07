bb4f97014a40f7318cb941db21df9565b70b907c
Bruno Almeida <brunoalmeida7426@gmail.com>
2025-02-07 19:47:38 +0000

    Some basic updates

--- src/main/java/com/example/books/BooksApplication.java
+++ src/main/java/com/example/books/BooksApplication.java
@@ -1,4 +1,4 @@
-package com.example.books.books;
+package com.example.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
