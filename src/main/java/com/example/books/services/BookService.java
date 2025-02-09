01890bba1bf2801dc662794c061f805e41ca03d9
Bruno Almeida <brunoalmeida7426@gmail.com>
2025-02-09 19:55:10 +0000

    Book service

--- src/main/java/com/example/books/services/BookService.java
+++ src/main/java/com/example/books/services/BookService.java
@@ -1,8 +1,12 @@
 package com.example.books.services;
 
+import java.util.Optional;
+
 import com.example.books.domain.Book;
 
 public interface BookService {
 
     Book create(Book book);
+
+    Optional<Book> findById(String isbn);
 }
